5d63a84228bcf872afc98ec694263a31a6ed5245
devast8a <devast8a@whitefirex.com>
2020-05-09 10:17:31 +0930

    Refactor Analysis pass

--- src/analysis.ts
+++ src/analysis.ts
@@ -1,4 +1,4 @@
-import { Thing, Tag, CallStatic, VariableFlags, Variable, GetVariable, Expr } from './ast';
+import { Function, Tag, CallStatic, VariableFlags, Variable, GetVariable, Expr, SetVariable } from './ast';
 import { Visitor, Register } from './ast/visitor';
 import { Compiler } from './compile';
 import { LoanViolationError, SimultaneousLoanError } from './errors';
@@ -6,14 +6,10 @@ import { LoanViolationError, SimultaneousLoanError } from './errors';
 export class Analyzer extends Visitor<State, void> {
     public compiler: Compiler;
 
-    // TODO: This really ought to be unique per function
-    public owned: Set<Variable>;
-
     public constructor(compiler: Compiler){
         super(setup, Visitor.VisitByDefault());
 
         this.compiler = compiler;
-        this.owned = new Set();
     }
 
     public check = super.visit;
@@ -22,6 +18,11 @@ export class Analyzer extends Visitor<State, void> {
 export module Analyzer {
     export class State {
 
+        public alive: Map<Variable, boolean>;
+
+        public constructor(){
+            this.alive = new Map();
+        }
     }
 }
 
@@ -34,6 +35,29 @@ export default Analyzer;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 function setup(reg: Register<Analyzer, State, void>){
 
+reg(Function, (thing, analyzer, state) => {
+    // Create a new state for functions
+    const functionState = new State();
+
+    for(const parameter of thing.parameters){
+        analyzer.check(parameter, functionState);
+    }
+
+    for(const stmt of thing.body){
+        analyzer.check(stmt, functionState);
+    }
+});
+
+reg(Variable, (thing, analyzer, state) => {
+    state.alive.set(thing, thing.value !== undefined);
+});
+
+reg(SetVariable, (thing, analyzer, state) => {
+    analyzer.check(thing.source, state);
+
+    state.alive.set(thing.target, true);
+});
+
 reg(CallStatic, (thing, analyzer, state) => {
     // Check that the golden rule is being followed by the program.
     //  "An object must not be mutated using a mutable loan while another loan could observe it"
@@ -45,143 +69,130 @@ reg(CallStatic, (thing, analyzer, state) => {
     //
     // Also check that lifetime rules are being followed.
     //  - Only alive objects are used.
-    // TODO: Actually check that only alive objects can be used
     //  - Mark objects as dead when they are moved.
     //  - Only owned objects can be moved.
-
-    interface Loan {
-        argument: Expr,
-        parameter: Variable,
-    };
 
     const immutable = new Map<Variable, Loan>();
     const mutable = new Map<Variable, Loan>();
 
 
-    // HACK: Omit analyzing for copy/move - we use them as part of ownership analysis
-    if(thing.target === analyzer.compiler.functions.copy){
-        return;
-    }
-    if(thing.target === analyzer.compiler.functions.move){
-        return;
-    }
+    const as = thing.arguments;
+    const ps = thing.target.parameters;
 
-    // TODO: We will need to review this code when we go to support variadic functions
-    const args = thing.arguments;
-    const params = thing.target.parameters;
+    for(let i = 0; i < as.length; i++){
+        // TODO: Support Fields/Arrays
 
-    for(let i = 0; i < args.length; i++){
-        const arg = args[i];
-        const param = params[i];
+        const argument = as[i];
+        const parameter = ps[i];
 
-        switch(param.flags & (VariableFlags.Mutates | VariableFlags.Owns)){
-            // Unmarked parameters are by default immutable.
-            default:
-                // TODO: Review this comment
-                // TODO: Support Fields/Arrays
-                // For now, the lifetime of mutable references can not exceed the lifetime of the function
-                //  call. So we know if the argument is not a variable expression, then it must be something
-                //  else and that something could not store a mutable reference.
-                if(arg.tag !== Tag.GetVariable){
-                    continue;
-                }
-
-                if(analyzer.owned.has(arg.variable)){
-                    analyzer.compiler.report(new LoanViolationError());
-                }
-
-                immutable.set(arg.variable, {
-                    argument: arg,
-                    parameter: params[i],
-                });
-                break;
-
-            // Marked as ONLY mutable.
-            // Check if a function mutably loans the same value more than once
-            case VariableFlags.Mutates:
-                // TODO: Review this comment
-                // For now, the lifetime of mutable references can not exceed the lifetime of the function
-                //  call. So we know if the argument is not a variable expression, then it must be something
-                //  else and that something could not store a mutable reference.
-                if(arg.tag !== Tag.GetVariable){
-                    continue;
-                }
-
-                // TODO: Don't merge these conditions. We should probably emit two different error types
-                if(mutable.has(arg.variable)){
-                    analyzer.compiler.report(new SimultaneousLoanError({
-                        call: thing,
-                        immutableArgument: arg,
-                        mutableArgument: arg,
-                        immutableParameter: params[i],
-                        mutableParameter: params[i],
-                    }));
-                }
-
-                if(analyzer.owned.has(arg.variable)){
-                    analyzer.compiler.report(new LoanViolationError());
-                }
-
-                mutable.set(arg.variable, {
-                    argument: arg,
-                    parameter: params[i],
-                });
-                break;
-
-            // Marked as owned or as both owned and mutable.
-            // From the caller, it does not matter whether the callee function mutates its owned parameter or not
-            case VariableFlags.Owns:
-            case VariableFlags.Owns | VariableFlags.Mutates:
-                // Must move or copy into an owned parameter
-                if(arg.tag !== Tag.CallStatic){
-                    // TODO: Must use move or copy
-                    analyzer.compiler.report(new LoanViolationError());
-                    continue;
-                }
-                if(arg.arguments.length !== 1 || arg.arguments[0].tag !== Tag.GetVariable){
-                    // TODO: Copy or move only allowed for variables
-                    analyzer.compiler.report(new LoanViolationError());
-                    continue;
-                }
-
-                const variable = (arg.arguments[0] as GetVariable).variable;
-
-                if(arg.target === analyzer.compiler.functions.copy){
-                    if(analyzer.owned.has(variable)){
-                        analyzer.compiler.report(new LoanViolationError());
-                    }
-                }
-
-                if(arg.target === analyzer.compiler.functions.move){
-                    if(immutable.has(variable) || mutable.has(variable) || analyzer.owned.has(variable)){
-                        analyzer.compiler.report(new LoanViolationError());
-                    }
-
-                    if((variable.flags & (VariableFlags.Local | VariableFlags.Owns)) === 0){
-                        // TODO: Can only move local variables or variables that are owned
-                        analyzer.compiler.report(new LoanViolationError());
-                    }
-
-                    analyzer.owned.add(variable);
-                }
-
-                break;
+        if(parameter.flags & VariableFlags.Owns){
+            HandleOwnedParameter(analyzer, state, argument);
+            continue;
         }
-    }
 
-    for(const i of immutable){
-        const m = mutable.get(i[0]);
-        if(m !== undefined){
-            analyzer.compiler.report(new SimultaneousLoanError({
-                call: thing,
-                immutableArgument: i[1].argument,
-                immutableParameter: i[1].parameter,
-                mutableArgument: m.argument,
-                mutableParameter: m.parameter,
-            }));
+        analyzer.check(argument, state);
+
+        if(argument.tag !== Tag.GetVariable){
+            continue;
+        }
+
+        if(!state.alive.get(argument.variable)){
+            // Variable isn't alive
+            analyzer.compiler.report(new LoanViolationError());
+            continue;
+        }
+
+        const loan = {argument, parameter};
+
+        if(parameter.flags & VariableFlags.Mutates){
+            // Check that a function does not loan a value mutably AND immutably
+            let other = immutable.get(argument.variable);
+            if(other !== undefined){
+                reportSimultaneousLoan(analyzer, thing, loan, other);
+            }
+
+            // Check that a function does not mutably loan the same value more than once
+            other = mutable.get(argument.variable);
+            if(other !== undefined){
+                reportSimultaneousLoan(analyzer, thing, loan, other);
+            }
+
+            mutable.set(argument.variable, loan);
+        } else {
+            // Check that a function does not loan a value mutably AND immutably
+            let other = mutable.get(argument.variable);
+            if(other !== undefined){
+                reportSimultaneousLoan(analyzer, thing, other, loan);
+            }
+
+            immutable.set(argument.variable, loan);
         }
     }
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+interface Loan {
+    argument: Expr,
+    parameter: Variable,
+};
+
+function isMove(expression: Expr, analyzer: Analyzer){
+    if(expression.tag !== Tag.CallStatic){
+        return false;
+    }
+
+    return expression.target === analyzer.compiler.functions.move;
+}
+
+function getCopyOrMoveTarget(expression: Expr, analyzer: Analyzer){
+    if(expression.tag !== Tag.CallStatic){
+        return null;
+    }
+
+    if(expression.target !== analyzer.compiler.functions.copy &&
+        expression.target !== analyzer.compiler.functions.move){
+        return null;
+    }
+
+    // TODO: Support moving fields/arrays etc...
+    return (expression.arguments[0] as GetVariable).variable;
+}
+
+
+function HandleOwnedParameter(analyzer: Analyzer, state: State, argument: Expr){
+    const variable = getCopyOrMoveTarget(argument, analyzer);
+
+    if(variable === null){
+        // Must use copy or move into an owned variable
+        // TODO: Add syntax sugar for function results
+        analyzer.compiler.report(new LoanViolationError());
+        return;
+    }
+
+    if(!state.alive.get(variable)){
+        // Variable isn't alive
+        analyzer.compiler.report(new LoanViolationError());
+        return;
+    }
+
+    if(isMove(argument, analyzer)){
+        // Moved variables are no longer alive in the scope
+        state.alive.set(variable, false);
+    }
+}
+
+function reportSimultaneousLoan(analyzer: Analyzer, call: CallStatic, mutable: Loan, immutable: Loan){
+    analyzer.compiler.report(new SimultaneousLoanError({
+        call: call,
+        immutableArgument: immutable.argument,
+        immutableParameter: immutable.parameter,
+        mutableArgument: mutable.argument,
+        mutableParameter: mutable.parameter,
+    }));
+}
+
+function reportMultipleMutates(analyzer: Analyzer, call: CallStatic, mutable: Loan, immutable: Loan){
+    analyzer.compiler.report(new LoanViolationError());
 }
