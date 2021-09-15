e97dee59789730fa0b6c0f376aba0407ccae554f
devast8a <devast8a@whitefirex.com>
2021-09-15 20:05:44 +0930

    Generate ExprCallField nodes

--- src/stages/nodeResolution.ts
+++ src/stages/nodeResolution.ts
@@ -8,15 +8,23 @@ export const resolveNodes = new Visitor({
             case Tag.ExprCall: {
                 const target = node.target;
 
-                if (target.tag !== Tag.ExprRefNode) {
-                    break;
-                }
+                switch (target.tag) {
+                    case Tag.Function:  // Already fully resolved
+                    case Tag.SymbolSet: // Resolved during Overload Resolution
+                        break;
 
-                if (target.node.tag !== Tag.SymbolSet) {
-                    throw new Error(`Not implemented yet`);
-                }
+                    case Tag.ExprRefNode:
+                        if (target.node.tag !== Tag.SymbolSet) { throw new Error('Not implemented yet'); }
+                        return new Nodes.ExprCallStatic(target.node, node.args);
 
-                return new Nodes.ExprCallStatic(target.node, node.args);
+                    case Tag.ExprGetField:
+                        // Or we could translate this into a flat call?
+                        // TODO: Do proper name lookup here
+                        return new Nodes.ExprCallField(target.object, target.field as any, node.args);
+
+                    default:
+                        throw new Error(`resolveNodes > ExprCall > ${Tag[target.tag]} not supported`);
+                }
             }
         }
 
