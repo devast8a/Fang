a1abc170842895567443774cb2d725859f8fd4c1
devast8a <devast8a@whitefirex.com>
2021-10-31 20:58:48 +1030

    Mark generic functions as abstract functions too

--- src/stages/markAbstractFunctions.ts
+++ src/stages/markAbstractFunctions.ts
@@ -3,12 +3,31 @@ import { createVisitor } from '../ast/visitor';
 import { Flags } from '../common/flags';
 import { Context, DeclFunctionFlags, DeclVariable, Node, Tag, Type } from '../nodes';
 
+/**
+ * Mark abstract functions.
+ * 
+ * Marks functions as abstract (ie. sets the flag DeclFunctionFlags.Abstract).
+ * A function is abstract if it
+ *  - Is generic (has a `generic` keyword declaring generic parameters)
+ *  - Has a parameter that is an abstract type (see `isAbstractType`)
+ * 
+ * The primary user of this information is the `instantiate` pass that instantiates all abstract functions.
+ * It uses the abstract flag to avoid instantiating inside the bodies of abstract functions and to detect when it should
+ *  instantiate the target of a function call.
+ */
 export const markAbstractFunctions = createVisitor(VisitDecls, (context, decl) => {
     if (decl.tag === Tag.DeclFunction) {
         // TODO: Create a cleaner way to query parameters
         if (decl.parameters.some(parameter => isAbstractType(context, Node.as(decl.children.nodes[parameter], DeclVariable).type))) {
             return Node.mutate(decl, {
-                flags: Flags.set(decl.flags, DeclFunctionFlags.Abstract)
+                flags: Flags.set(decl.flags, DeclFunctionFlags.Abstract),
+            });
+        }
+
+        // TODO: Create a cleaner way to mark functions as generic
+        if (decl.generics !== null && decl.generics.parameters.length > 0) {
+            return Node.mutate(decl, {
+                flags: Flags.set(decl.flags, DeclFunctionFlags.Abstract),
             });
         }
     }
